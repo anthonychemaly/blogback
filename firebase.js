34de2c96ee8f876da97681a69de663c0a07ccb8d
anthonychemaly <anthonychemalygr@gmail.com>
2021-04-27 19:46:05 +0300

    react-create-app on Heroku

--- firebase.js
+++ firebase.js
@@ -1,21 +1,21 @@
-const admin = require("firebase-admin");
+// const admin = require("firebase-admin");
 
-// Initialize firebase admin SDK
-var firebaseConfig = {
-  apiKey: "AIzaSyCmrf3WUwUy5H1P-onTWVK8lCHdXq2yU2c",
-  authDomain: "blog-57c3e.firebaseapp.com",
-  projectId: "blog-57c3e",
-  storageBucket: "blog-57c3e.appspot.com",
-  messagingSenderId: "98330484483",
-  appId: "1:98330484483:web:b360ef49b3d34192319336",
-  measurementId: "G-RNS35E7QDQ",
-};
-// Initialize Firebase
-admin.initializeApp(firebaseConfig);
+// // Initialize firebase admin SDK
+// var firebaseConfig = {
+//   apiKey: "AIzaSyCmrf3WUwUy5H1P-onTWVK8lCHdXq2yU2c",
+//   authDomain: "blog-57c3e.firebaseapp.com",
+//   projectId: "blog-57c3e",
+//   storageBucket: "blog-57c3e.appspot.com",
+//   messagingSenderId: "98330484483",
+//   appId: "1:98330484483:web:b360ef49b3d34192319336",
+//   measurementId: "G-RNS35E7QDQ",
+// };
+// // Initialize Firebase
+// admin.initializeApp(firebaseConfig);
 
-// Cloud storage
-const bucket = admin.storage().bucket();
+// // Cloud storage
+// const bucket = admin.storage().bucket();
 
-module.exports = {
-  bucket,
-};
+// module.exports = {
+//   bucket,
+// };
