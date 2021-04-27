2088ed0ccb79e5c3b7b85d2f0800dd0edfff9726
anthonychemaly <anthonychemalygr@gmail.com>
2021-04-27 09:12:31 +0300

    react-create-app on Heroku

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var apis = require("../controllers/admin.controller");
 
-router.post("/admin/login", apis.AdminLogin);
-router.post("/admin/register", apis.AdminRegister);
+router.post("/login", apis.AdminLogin);
+router.post("/register", apis.AdminRegister);
 
 module.exports = router;
