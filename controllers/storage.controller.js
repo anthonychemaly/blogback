1e660e618bb128b1db2c1a736a129e14f0518838
anthonychemaly <anthonychemalygr@gmail.com>
2021-04-27 18:16:43 +0300

    react-create-app on Heroku

--- controllers/storage.controller.js
+++ controllers/storage.controller.js
@@ -2,47 +2,49 @@ const path = require("path");
 var jwt_decode = require("jwt-decode");
 var Media = require("../models/media");
 const Admin = require("../models/admin");
-var firebase = require("firebase/app");
-// import "firebase/storage"; 
+const firebase = require("../firebase");
 
-// const storageRef = firebase.storage().ref();
+exports.UploadProfilePic = async (req, res) => {
+  if (!req.file) {
+    res.status(400).send("Error: No files found");
+  } else {
+    const blob = firebase.bucket.file(req.files.file);
 
-// exports.UploadProfilePic = async (req, res) => {
-//   const file = req.files.upload;
-//   storageRef.put(file).then((snapshot) => {
-//     console.log("Uploaded a blob or file!");
-//   });
-  //   var token = req.body.token || req.query.token || req.headers["token"];
-  //   var decodedtoken = jwt_decode(token);
-  //   uploadFile(bucketName, filePath, destFileName);
-  //   file.mv(`./public/images/${file.name}`, (err, result) => {
-  //     if (err) res.send(err);
+    const blobWriter = blob.createWriteStream({
+      metadata: {
+        contentType: file.mimetype,
+      },
+    });
 
-  //     var newMedia = new Media({
-  //       type: file.mimetype,
-  //       url: `https://blogback.herokuapp.com/images/${file.name}`,
-  //       fileName: file.name,
-  //       admin: decodedtoken.id,
-  //       created_at: new Date(),
-  //     });
+    blobWriter.on("error", (err) => {
+      console.log(err);
+    });
 
-  //     newMedia.save().then((mediaData) => {
-  //       if (err) res.send(err);
-  //       res.send({
-  //         success: true,
-  //         data: mediaData,
-  //       });
-  //   Admin.findByIdAndUpdate(
-  //     decodedtoken.id,
-  //     { picture: mediaData._id },
-  //     (err, adminData) => {
-  //       if (err) res.send(err);
-  //       res.send({
-  //         success: true,
-  //         data: adminData,
-  //       });
-  //     }
-  //   );
-  //     });
-  //   });
-// };
+    blobWriter.on("finish", () => {
+      res.status(200).send("File uploaded.");
+      var newMedia = new Media({
+        type: file.mimetype,
+        //   url: `https://blogback.herokuapp.com/images/${file.name}`,
+        fileName: file.name,
+        admin: decodedtoken.id,
+        created_at: new Date(),
+      });
+
+      newMedia.save().then((mediaData) => {
+        Admin.findByIdAndUpdate(
+          decodedtoken.id,
+          { picture: mediaData._id },
+          (err, adminData) => {
+            if (err) res.send(err);
+            res.send({
+              success: true,
+              data: adminData,
+            });
+          }
+        );
+      });
+    });
+
+    blobWriter.end(req.file.buffer);
+  }
+};
