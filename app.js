702fdf42e8092362a4ab30eea7ecfebe56aa1342
anthonychemaly <anthonychemalygr@gmail.com>
2021-04-27 19:25:18 +0300

    react-create-app on Heroku

--- app.js
+++ app.js
@@ -61,23 +61,24 @@ app.post("/upload/profile", upload.single("file"), (req, res) => {
   if (!req.file) {
     res.status(400).send("Error: No files found");
   } else {
-    const blob = firebase.bucket.file(req.file.originalname);
+    console.log(req.file.filename)
+    // const blob = firebase.bucket.file(req.file.originalname);
 
-    const blobWriter = blob.createWriteStream({
-      metadata: {
-        contentType: req.file.mimetype,
-      },
-    });
+    // const blobWriter = blob.createWriteStream({
+    //   metadata: {
+    //     contentType: req.file.mimetype,
+    //   },
+    // });
 
-    blobWriter.on("error", (err) => {
-      console.log(err);
-    });
+    // blobWriter.on("error", (err) => {
+    //   console.log(err);
+    // });
 
-    blobWriter.on("finish", () => {
-      res.status(200).send("File uploaded.");
-    });
+    // blobWriter.on("finish", () => {
+    //   res.status(200).send("File uploaded.");
+    // });
 
-    blobWriter.end(req.file.buffer);
+    // blobWriter.end(req.file.buffer);
   }
 });
 
