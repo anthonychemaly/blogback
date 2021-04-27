1aabc5fa27c95c016cf0e329874ede6c3646bad5
anthonychemaly <anthonychemalygr@gmail.com>
2021-04-27 18:49:21 +0300

    react-create-app on Heroku

--- routes/storage.js
+++ routes/storage.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var apis = require("../controllers/storage.controller");
+const firebase = require("../firebase");
 const multer = require("multer");
 
 const upload = multer({
