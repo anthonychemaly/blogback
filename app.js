452d77899b7e8e3832da86f58d5cd2a08a92f214
anthonychemaly <anthonychemalygr@gmail.com>
2021-04-27 12:45:26 +0300

    react-create-app on Heroku

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var cors = require("cors");
+var firebase = require("firebase/app");
 
 var indexRouter = require("./routes/index");
 var adminsRouter = require("./routes/admin");
@@ -14,6 +15,20 @@ var blogsRouter = require("./routes/blog");
 var fileupload = require("express-fileupload");
 
 var app = express();
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCmrf3WUwUy5H1P-onTWVK8lCHdXq2yU2c",
+  authDomain: "blog-57c3e.firebaseapp.com",
+  projectId: "blog-57c3e",
+  storageBucket: "blog-57c3e.appspot.com",
+  messagingSenderId: "98330484483",
+  appId: "1:98330484483:web:b360ef49b3d34192319336",
+  measurementId: "G-RNS35E7QDQ",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 app.use(cors());
 
 mongoose.connect(
@@ -28,7 +43,7 @@ mongoose.connect(
   }
 );
 
- // view engine setup
+// view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
--- controllers/storage.controller.js
+++ controllers/storage.controller.js
@@ -2,40 +2,49 @@ const path = require("path");
 var jwt_decode = require("jwt-decode");
 var Media = require("../models/media");
 const Admin = require("../models/admin");
+var firebase = require("firebase");
+// Get a reference to the storage service, which is used to create references in your storage bucket
+var storage = firebase.storage();
+
+// Create a storage reference from our storage service
+var storageRef = storage.ref();
 
 exports.UploadProfilePic = async (req, res) => {
   const file = req.files.upload;
-  var token = req.body.token || req.query.token || req.headers["token"];
-  var decodedtoken = jwt_decode(token);
-
-  file.mv(`./public/images/${file.name}`, (err, result) => {
-    if (err) res.send(err);
-
-    var newMedia = new Media({
-      type: file.mimetype,
-      url: `https://blogback.herokuapp.com/images/${file.name}`,
-      fileName: file.name,
-      admin: decodedtoken.id,
-      created_at: new Date(),
-    });
-
-    newMedia.save().then((mediaData) => {
-      if (err) res.send(err);
-      res.send({
-        success: true,
-        data: mediaData,
-      });
-      //   Admin.findByIdAndUpdate(
-      //     decodedtoken.id,
-      //     { picture: mediaData._id },
-      //     (err, adminData) => {
-      //       if (err) res.send(err);
-      //       res.send({
-      //         success: true,
-      //         data: adminData,
-      //       });
-      //     }
-      //   );
-    });
+  storageRef.put(file).then((snapshot) => {
+    console.log("Uploaded a blob or file!");
   });
+  //   var token = req.body.token || req.query.token || req.headers["token"];
+  //   var decodedtoken = jwt_decode(token);
+  //   uploadFile(bucketName, filePath, destFileName);
+  //   file.mv(`./public/images/${file.name}`, (err, result) => {
+  //     if (err) res.send(err);
+
+  //     var newMedia = new Media({
+  //       type: file.mimetype,
+  //       url: `https://blogback.herokuapp.com/images/${file.name}`,
+  //       fileName: file.name,
+  //       admin: decodedtoken.id,
+  //       created_at: new Date(),
+  //     });
+
+  //     newMedia.save().then((mediaData) => {
+  //       if (err) res.send(err);
+  //       res.send({
+  //         success: true,
+  //         data: mediaData,
+  //       });
+  //   Admin.findByIdAndUpdate(
+  //     decodedtoken.id,
+  //     { picture: mediaData._id },
+  //     (err, adminData) => {
+  //       if (err) res.send(err);
+  //       res.send({
+  //         success: true,
+  //         data: adminData,
+  //       });
+  //     }
+  //   );
+  //     });
+  //   });
 };
